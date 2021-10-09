45788176fba6935a7a6eb3fa1645316d1be2c2b5
kaydenmok <kaydenmokrytzki@gmail.com>
2021-10-09 13:43:49 -0400

    password

--- lessons/matrixKeypad.py
+++ lessons/matrixKeypad.py
@@ -6,6 +6,8 @@ keys = ['1','2','3','A','4','5','6','B','7','8','9','C','*','0','#','D']
 
 rowsPins = [12, 16,18,22]
 colsPins = [19,15,13,11]
+
+
 def loop():
     keypad = Keypad.Keypad(keys,rowsPins,colsPins, ROWS,COLS)
     keypad.setDebounceTime(50)
@@ -13,6 +15,12 @@ def loop():
         key = keypad.getKey()
         if(key != keypad.NULL):
             print("You Pressed Key : %c "%(key))
+
+
+
+        
+
+
 if __name__ == '__main__':
     print("Program is Starting")
     try:
@@ -22,55 +30,5 @@ if __name__ == '__main__':
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        #done
+# Type right password + enter => Green LED is on, otherwise Red LED is on
+# Add new password/Reset password if the green LED is on (not Red LED)
