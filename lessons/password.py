34988f2e70802edbb93c3cf655372521234adb21
kaydenmok <kaydenmokrytzki@gmail.com>
2021-10-09 15:54:16 -0400

    updated

--- lessons/password.py
+++ lessons/password.py
@@ -3,7 +3,7 @@ import Keypad
 ROWS = 4
 COLS = 4
 keys = ['1','2','3','A','4','5','6','B','7','8','9','C','*','0','#','D']
-
+password = ['1', '2', '3']
 rowsPins = [12, 16,18,22]
 colsPins = [19,15,13,11]
 ledPins = [36, 38]   #36 (16)= red, 38 (20) = green
@@ -28,6 +28,9 @@ def loop():
                     print('Pass')
                     GPIO.output(ledPins[1], GPIO.HIGH)
                     GPIO.output(ledPins[0], GPIO.LOW)
+                elif(key == 'A'):
+                    password = updatePassword(keypad)
+                    print("password updated")
                 else:
                     print('Wrong password')
                     GPIO.output(ledPins[0], GPIO.HIGH)
@@ -43,7 +46,7 @@ def loop():
 # Doing: Matching the passwords
 # Output: return true or false
 def checkPassword(keyPress):
-    password = ['1', '2', '3']
+
     matches = [i for i, j in zip(password, keyPress) if i == j]
     print(matches)
     if len(matches) == len(password) == len(keyPress):
@@ -51,8 +54,19 @@ def checkPassword(keyPress):
     else:
         return False
         
-    
+def updatePassword(keypad):
+    newPassword = []
 
+    # The loop to catch all the typed keypad until enter
+    while(True):
+        key = keypad.getKey()
+        if(key != keypad.NULL):
+            if(key == 'D'):
+                break
+            else:
+                newPassword.append(key)
+    # return new password
+    return newPassword
 
 if __name__ == '__main__':
     setup()
