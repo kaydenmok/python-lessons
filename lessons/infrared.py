1d6c4d685de069151fca9a7cccd28ad892c319e8
kaydenmok <kaydenmokrytzki@gmail.com>
2021-10-16 14:12:21 -0400

    DONE

---
+++ lessons/infrared.py
@@ -0,0 +1,30 @@
+import RPi.GPIO as GPIO
+
+ledPin = 12
+sensorPin = 11
+
+def setup():
+    print('starting...')
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(ledPin, GPIO.OUT)
+    GPIO.setup(sensorPin, GPIO.IN)
+
+def loop():
+    while True:
+        if GPIO.input(sensorPin)==GPIO.HIGH:
+            GPIO.output(ledPin, GPIO.HIGH)
+            print('led on...')
+
+        else:
+            GPIO.output(ledPin, GPIO.LOW)
+            print('led off...')
+
+def destroy():
+    GPIO.cleanup()
+if __name__ == '__main__':
+    setup()
+    try:
+        loop()
+    except KeyboardInterrupt:
+        destroy()
+        
