d28d7f40d745bf5af102d64ef56419235387b06c
kaydenmok <kaydenmokrytzki@gmail.com>
2021-10-30 13:54:50 -0400

    done

---
+++ lessons/sound.py
@@ -0,0 +1,30 @@
+import RPi.GPIO as GPIO
+import time
+D0=12
+LED = 11
+def setup():
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(D0, GPIO.IN)
+    GPIO.setup(LED, GPIO.OUT)
+    
+def loop():
+    while True:
+        print('shock:')
+        PIN = GPIO.input(D0)
+        if PIN == GPIO.HIGH:
+            GPIO.output(LED, GPIO.HIGH)
+            print('LED... HIGH')
+        else:
+            GPIO.output(LED,GPIO.LOW)
+            print('LED... LOW')
+        time.sleep(0.5)
+
+def destroy():
+    GPIO.cleanup()
+
+if __name__ == '__main__':
+    setup()
+    try:
+        loop()
+    except KeyboardInterrupt:
+        destroy()
